1ea3f5ad10ebdaf901299c0020e107f11ba945d4
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-02-01 10:55:17 +0100

    add second part

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import PrimeiraParte from './PrimeiraParte/PrimeiraParte';
+import SegundaParte from './SegundaParte/SegundaParte';
 
 function App() {
   return (
     <div className="App">
        <PrimeiraParte />
+       <SegundaParte />
     </div>
     
   );
--- my-app/src/SegundaParte/SegundaParte.js
+++ my-app/src/SegundaParte/SegundaParte.js
@@ -1,9 +1,39 @@
-import
+import image1 from '../images/icon1.png'
+import image2 from '../images/icon2.png'
+import image3 from '../images/icon3.png'
+import image4 from '../images/icon4.png'
 
 import './SegundaParte.css'
 
 function SegundaParte() {
     return (
+        <div class="secondDiv">
+            <section>
+                <img src={image1} alt="image1"/>
+                <h4> Declarative </h4>
+                <p> React makes it painless to create interactie UIs. </p>
+            </section>
+
+            <section>
+                <img src={image2} alt="image2"/>
+                <h4> Components </h4>
+                <p> Build encapsulated components that manage their state. </p>
+            </section>
+
+            <section>
+                <img src={image3} alt="image3"/>
+                <h4> Single-Way </h4>
+                <p> A set of immutable values are passed to component's. </p>
+            </section>
+
+            <section>
+                <img src={image4} alt="image4"/>
+                <h4> JSX </h4>
+                <p> Statically-typed,designed to run on modern browsers. </p>
+            </section>
+        </div>
 
     )
 }
+
+export default SegundaParte
